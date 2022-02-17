f0e30bb66d3154547ccd2b2f9216f212793ab843
codewurld <sholaq@gmail.com>
2022-02-17 19:09:20 +0000

    created basic layout with express-handler

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path')
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
+const PORT = process.env.PORT || 8080
 
 // load config
-dotenv.config({ path: './config/config.env' })
-
-const app = express();
-
-const PORT = process.env.PORT || 8080
+dotenv.config({ path: './config/config.env' });
 
 // connect app to DB
 connectDB();
+
+const app = express();
+
+// logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+};
+
+// handlebars view engine
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Routes - all / requests connects to index file
+app.use('/', require('./routes/index'));
+
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+// top level routes
+
+const express = require('express');
+const router = express.Router()
+
+// @desc Login/Landing page
+// @route GET /
+router.get('/', (req, res) => {
+    // looks for templates called dashboard in views folder
+    res.render('login')
+})
+
+// @desc Dashboard
+// @route GET /dashboard
+router.get('/dashboard', (req, res) => {
+    // looks for templates called dashboard in views folder
+    res.render('dashboard');
+})
+
+
+module.exports = router
