a5420bd46a14d3c61b073d0c623bac9e56df637e
codewurld <sholaq@gmail.com>
2022-02-22 20:54:40 +0000

    added stories form

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes - all / requests connects to index file
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 
 
