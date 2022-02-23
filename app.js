3852a726df38762568cd37b0ce25674952f94778
codewurld <sholaq@gmail.com>
2022-02-23 19:21:56 +0000

    added story update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const methodOverride = require('method-override');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 8084
@@ -26,13 +27,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
+// Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 // logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 };
 
 // Handlebars Helpers
-const { formatDate, truncate, stripTags, editIcon } = require('./helpers/hbs');
+const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/hbs');
 
 // handlebars view engine
 app.engine('.hbs', exphbs.engine({
@@ -40,7 +51,8 @@ app.engine('.hbs', exphbs.engine({
         formatDate,
         stripTags,
         truncate,
-        editIcon
+        editIcon,
+        select
     },
     defaultLayout: 'main', extname: '.hbs'
 }));
